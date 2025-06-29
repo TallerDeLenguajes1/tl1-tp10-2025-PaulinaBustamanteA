046addfd164026509ab26f643b1da2210d453c05
Tatiana Paulina Bustamante Arias <paulinaarias2205@gmail.com>
2025-06-29 20:55:05 -0300

    miwebapp

---
+++ MiWebApi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.IO;
+
+class DatoGato
+{
+    public string Fact { get; set; }
+}
+
+class Program
+{
+    static async Task Main()
+    {
+        HttpClient client = new HttpClient();
+
+        Console.WriteLine("Obteniendo dato curioso sobre gatos...");
+
+        string respuesta = await client.GetStringAsync("https://catfact.ninja/fact");
+
+        // Convertimos el JSON en un objeto
+        DatoGato dato = JsonSerializer.Deserialize<DatoGato>(respuesta);
+
+        Console.WriteLine("\n--- DATO SOBRE GATOS ---");
+        Console.WriteLine(dato.Fact);
+
+        // Guardamos el dato en un archivo JSON
+        File.WriteAllText("datoGato.json", respuesta);
+
+        Console.WriteLine("\nSe guardó el dato en datoGato.json");
+    }
+}
